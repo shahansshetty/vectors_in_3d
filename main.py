e616dab636113b00f338943b3065d7c52c510714
Shahan S Shetty <shahansshetty@gmail.com>
2025-08-06 19:48:58 +0530

    added legend in the graph and also displays sum.

--- main.py
+++ main.py
@@ -2,45 +2,42 @@ import matplotlib.pyplot as plt
 # import numpy as np
 # from mpl_toolkits import mplot3d
 
-
-# a=[3,4,2]
-# z=[4,5,1]
-
-# y=np.arange(0,50,0.1)
-axes=plt.axes(projection='3d')
-
-def v_sum(a,b):
-    sum=list()
-    for i in range(len(a)):
-        sum+=[a[i]+b[i]]
-    largest=max(sum)
-    return sum,largest
-
 print('3d vector addition')
 print('values of a ')
 a=[int(input(f'enter the {chr(120+i)} coordinate of a :'))for i in range(3)]
 print('values of b ')
 b=[int(input(f'enter the {chr(120+i)} coordinate of b :'))for i in range(3)]
 
-# if len(a) or len(z)!=3:
-#     print('size of the a and b is not 3')
-#     exit(0)
+# def draw_3d(a,b,sum,largest):
+#     axes=plt.axes(projection='3d')
+#     axes.quiver(0,0,0,a[0],a[1],a[2],color='orange',arrow_length_ratio=0.1)
+#     axes.quiver(0,0,0,b[0],b[1],b[2],color='red',arrow_length_ratio=0.1)
+#     axes.quiver(0,0,0,sum[0],sum[1],sum[2],color='green',arrow_length_ratio=0.1)
+#     axes.set_xlim([-largest,largest]) 
+#     axes.set_ylim([-largest,largest])
+#     plt.show() 
 
-sum,largest=v_sum(a,b)
-print(sum,largest)
-# print('Here are the vector operatins avalible')
-# print('1.add')
-# print('2.multipy')
-# print('3.divide')
+sum=list()
+for i in range(len(a)):
+    sum+=[a[i]+b[i]]
+    largest=max(sum)
+# draw_3d(a,b,sum,largest)
+print(sum,a,b)
+print(largest)
+
+axes=plt.axes(projection='3d')
+axes.quiver(0,0,0,a[0],a[1],a[2],color='orange',arrow_length_ratio=0.1,label='a')
+axes.quiver(0,0,0,b[0],b[1],b[2],color='red',arrow_length_ratio=0.1,label='b')
+axes.quiver(0,0,0,sum[0],sum[1],sum[2],color='green',arrow_length_ratio=0.1,label='sum')
+axes.set_xlim([-largest,largest]) 
+axes.set_ylim([-largest,largest])
+axes.set_zlim([-largest,largest])
+axes.legend()
+plt.title(f'3D_VECTOR_ADDITION , sum:{sum}')
+
+
+plt.show() 
 
 
 
 
-# axes.quiver(0,0,0,a[0],a[1],a[2],color='orange',arrow_length_ratio=0.1)
-# axes.quiver(0,0,0,z[0],z[1],z[2],color='red',arrow_length_ratio=0.1)
-# axes.quiver(0,0,0,sum[0],sum[1],sum[2],color='green',arrow_length_ratio=0.1)
-
-# axes.set_xlim([-largest,largest]) 
-# axes.set_ylim([-largest,largest])
-# axes.set_zlim([-largest,largest]) 
-# plt.show() 
